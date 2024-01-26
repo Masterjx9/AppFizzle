69f6c769b707484ab90fcbcc9caf63e3d1b6bd29
Jaton Justice <masterjx9@gmail.com>
2024-01-25 20:50:24 -0800

    updated regex

--- flask_backend/common/__init__.py
+++ flask_backend/common/__init__.py
@@ -13,78 +13,68 @@ def get_config(folder_name, directory_path):
                     return config_file.read()
     return None
 
-backend_data_test = json.loads(get_config("asp_net_core", "backends"))
-backend_requirements = backend_data_test["requirements"]
-backend_dockerfile = ""
-for requirement in backend_requirements:
-    # print("retreiving requirement: " + requirement)
-    # print(backend_data_test[requirement])
-    for line in backend_data_test[requirement]:
-        backend_dockerfile += line + "\n"
-# print(backend_dockerfile)
-# print("")
-# print("")
-# print("")
-frontend_data_test = json.loads(get_config("angular", "frontends"))
-frontend_requirements = frontend_data_test["requirements"]
-frontend_dockerfile = ""
-for requirement in frontend_requirements:
-    # print("retreiving requirement: " + requirement)
-    # print(frontend_data_test[requirement])
-    for line in frontend_data_test[requirement]:
-        frontend_dockerfile += line + "\n"
-# print(frontend_dockerfile)
-
-
-def replace_template_variables(backend_dockerfile, defaults, frontend_dockerfile=None, frontend_data=None, user_values=None):
-    # Function to replace template variables with provided values
-    def replace_variables(dockerfile, data_source):
-        variable_pattern = r"\{\{ (.*?) \}\}"
-        variable_matches = re.findall(variable_pattern, dockerfile)
-        for variable in variable_matches:
-            value = data_source.get(variable, '')
-            dockerfile = re.sub(r"\{\{\s*" + variable + r"\s*\}\}", value, dockerfile)
-        return dockerfile
-
-    # Handle #each directives in frontend_dockerfile
-    if frontend_dockerfile:
-        frontend_dockerfile = process_each_directive(frontend_dockerfile, frontend_data["defaults"])
-        frontend_dockerfile = replace_variables(frontend_dockerfile, frontend_data["defaults"])
-    
-    # Concatenate frontend and backend Dockerfiles
-    dockerfile_combined = frontend_dockerfile + backend_dockerfile if frontend_dockerfile else backend_dockerfile
-
-    # Process #each directives in the combined Dockerfile
-    dockerfile_combined = process_each_directive(dockerfile_combined, defaults)
-
-    # Process #if directives
-    if_pattern = r"\{\{\#if (.*?)\}\}(.*?)\{\{\/if\}\}"
-    if_matches = re.finditer(if_pattern, dockerfile_combined, re.DOTALL)
-    for match in if_matches:
-        condition, content = match.groups()
-        if condition == "frontend" and frontend_data:
-            content = replace_variables(content, frontend_data.get('defaults', {}))
-            dockerfile_combined = dockerfile_combined.replace(match.group(0), content)
-        else:
-            dockerfile_combined = dockerfile_combined.replace(match.group(0), '')
-
-    # Replace remaining variables in the combined Dockerfile
-    dockerfile_combined = replace_variables(dockerfile_combined, user_values if user_values else defaults)
-
-    return dockerfile_combined
-
-def process_each_directive(dockerfile, data_source):
-    each_pattern = r"\{\{\#each (.*?)\}\}(.*?)\{\{\/each\}\}"
-    each_matches = re.finditer(each_pattern, dockerfile, re.DOTALL)
-    for match in each_matches:
-        list_variable, content = match.groups()
-        items = data_source.get(list_variable.split('.')[1], [])
-        replacement = ''.join([re.sub(r"\{\{this\}\}", item, content) for item in items])
-        dockerfile = dockerfile.replace(match.group(0), replacement)
+def build_dockerfile(config):
+    dockerfile = ""
+    for requirement in config["requirements"]:
+        for line in config[requirement]:
+            dockerfile += line + "\n"
     return dockerfile
 
-# Example Usage
-user_values = {"version": "3.1", "project_name": "MyApp"}  # Example user provided values
+def replace_template_variables(backend_dockerfile, backend_defaults, frontend_dockerfile=None, frontend_data=None, user_values=None):
+    each_pattern = r"\{\{\#each (.*?)\}\}(.*?)\{\{\/each\}\}"
+    if_pattern = r"\{\{\#if (.*?)\}\}(.*?)\{\{\/if\}\}"
+    variable_pattern = r"\{\{ (.*?) \}\}"
+    
+    if frontend_dockerfile is not None:
+        ft_each_matches = re.finditer(each_pattern, frontend_dockerfile, re.DOTALL)
+        for match in ft_each_matches:
+            list_variable, content = match.groups()
+            items = frontend_data["defaults"].get(list_variable.split('.')[1], [])
+            replacement = ''.join([re.sub(r"\{\{this\}\}", item, content) for item in items])
+            frontend_dockerfile = frontend_dockerfile.replace(match.group(0), replacement)
+        
+        ft_variable_matches = re.findall(variable_pattern, frontend_dockerfile)
+        for variable in ft_variable_matches:
+            value = user_values.get(variable, frontend_data["defaults"].get(variable, '')) if user_values is not None else frontend_data["defaults"].get(variable, '')
+            frontend_dockerfile = re.sub(r"\{\{\s*" + variable + r"\s*\}\}", value, frontend_dockerfile)
+    
+    backend_dockerfile = frontend_dockerfile + backend_dockerfile
+    
+    bk_each_matches = re.finditer(each_pattern, backend_dockerfile, re.DOTALL)
+    for match in bk_each_matches:
+        list_variable, content = match.groups()
+        items = backend_defaults.get(list_variable.split('.')[1], [])
+        replacement = ''.join([re.sub(r"\{\{this\}\}", item, content) for item in items])
+        backend_dockerfile = backend_dockerfile.replace(match.group(0), replacement)
+
+    if_matches = re.finditer(if_pattern, backend_dockerfile, re.DOTALL)
+    for match in if_matches:
+        condition, content = match.groups()
+        include_content = False
+        if condition == "frontend" and frontend_data is not None:
+            include_content = True
+            while re.search(variable_pattern, content):
+                for variable in re.findall(variable_pattern, content):
+                    if 'frontend.' in variable:
+                        variable_key = variable.split('frontend.')[1]
+                        value = frontend_data.get(variable_key, '')
+                    else:
+                        value = frontend_data["defaults"].get(variable, '') if user_values is None else user_values.get(variable, frontend_data["defaults"].get(variable, ''))
+                    content = re.sub(r"\{\{\s*" + variable + r"\s*\}\}", value, content)
+            backend_dockerfile = backend_dockerfile.replace(match.group(0), content if include_content else "")
+
+    bk_variable_matches = re.findall(variable_pattern, backend_dockerfile)
+    for variable in bk_variable_matches:
+        value = user_values.get(variable, backend_defaults.get(variable, '')) if user_values is not None else backend_defaults.get(variable, '')
+        backend_dockerfile = re.sub(r"\{\{\s*" + variable + r"\s*\}\}", value, backend_dockerfile)
+
+    return backend_dockerfile
+
+backend_data_test = json.loads(get_config("asp_net_core", "backends"))
+backend_dockerfile = build_dockerfile(backend_data_test)
+
+frontend_data_test = json.loads(get_config("angular", "frontends"))
+frontend_dockerfile = build_dockerfile(frontend_data_test)
 
 processed_dockerfile = replace_template_variables(backend_dockerfile, backend_data_test["defaults"], frontend_dockerfile, frontend_data_test)
 print(processed_dockerfile)
