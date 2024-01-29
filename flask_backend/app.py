9ff7e98b897384af14252ca1ef696f928f31e0b7
Jaton Justice <masterjx9@gmail.com>
2024-01-29 10:11:29 -0800

    created dockerfilepackage flow

--- flask_backend/app.py
+++ flask_backend/app.py
@@ -1,26 +1,53 @@
-from flask import Flask, request
-import json
+from flask import Flask, request, send_file, g
 import common
-
+import uuid
+import os
+import shutil
 app = Flask(__name__)
 
-@app.route('/builder', methods=['POST'])
+@app.route('/builder_dockerfile', methods=['POST'])
 def builder():
     if request.method == 'POST':
         data = request.json
         print(data)
-        backend_config = json.loads(common.get_config(data["backend"], "backends"))
-        backend_dockerfile = common.build_dockerfile(backend_config)
-        
         if "frontend" in data:
-            frontend_config = json.loads(common.get_config(data["frontend"], "frontends"))
-            frontend_dockerfile = common.build_dockerfile(frontend_config)
+            processed_dockerfile = common.dockerfile_flow(data["backend"], data["frontend"])
         else:
-            frontend_config = None
-            frontend_dockerfile = None
-        
-        processed_dockerfile = common.replace_template_variables(backend_dockerfile, backend_config["defaults"], frontend_dockerfile, frontend_config)
+            processed_dockerfile = common.dockerfile_flow(data["backend"])
         return {"result": "success", "dockerfile": processed_dockerfile}
     
+@app.route('/builder_dockerpackage', methods=['POST'])
+def builder_dockerpackage():
+    if request.method == 'POST':
+        data = request.json
+        print(data)
+        if "frontend" in data:
+            processed_dockerfile = common.dockerfile_flow(data["backend"], data["frontend"])
+        else:
+            processed_dockerfile = common.dockerfile_flow(data["backend"])
+            
+        unique_id = uuid.uuid4()
+        g.unique_id = unique_id  # Store the unique_id in g for later cleanup
+        packaged_dockerfile = common.package_dockerfile(unique_id, data, processed_dockerfile)
+        
+        # Ensure the file exists
+        if os.path.exists(packaged_dockerfile):
+            return send_file(packaged_dockerfile, as_attachment=True, download_name=f"dockerfile_package_{unique_id}.zip")
+        else:
+            return {"error": "Packaged Dockerfile not found."}, 404
+
+    
+@app.after_request
+def cleanup(response):
+    unique_id = getattr(g, 'unique_id', None)
+    if unique_id:
+        temp_dir = os.path.join(os.getcwd(), "temp", str(unique_id))
+        if os.path.isdir(temp_dir):
+            print("Removing directory:", temp_dir)
+            shutil.rmtree(temp_dir)  # Delete the entire directory tree
+            g.unique_id = None # Reset the flag
+
+    return response
+
 if __name__ == '__main__':
     app.run(debug=True)    
--- flask_backend/common/__init__.py
+++ flask_backend/common/__init__.py
@@ -1,6 +1,68 @@
 import os
 import re
+import json
+import zipfile
+import shutil
 
+def dockerfile_flow(data_backend, data_frontend=None):
+    backend_config = json.loads(get_config(data_backend, "backends"))
+    backend_dockerfile = build_dockerfile(backend_config)
+    if data_frontend is not None:
+        frontend_config = json.loads(get_config(data_frontend, "frontends"))
+        frontend_dockerfile = build_dockerfile(frontend_config)
+    else:
+        frontend_config = None
+        frontend_dockerfile = None
+    processed_dockerfile = replace_template_variables(backend_dockerfile, backend_config["defaults"], frontend_dockerfile, frontend_config)        
+    return processed_dockerfile
+
+def copy_generics(unique_id, framework_name, framework_generics_path, backend_target_directory):
+    # Construct the path to the framework's generics directory directly
+    generics_dir = os.path.join(os.getcwd(), framework_generics_path, framework_name, "generics")
+
+    # Create a temporary directory with the unique_id
+    temp_dir = os.path.join(os.getcwd(), "temp", str(unique_id))
+    os.makedirs(temp_dir, exist_ok=True)
+
+    # Copy the backend's src folder to the temporary directory
+    backend_src_dir = os.path.join(os.getcwd(), "backends", backend_target_directory, "src")
+    if os.path.exists(backend_src_dir):
+        shutil.copytree(backend_src_dir, os.path.join(temp_dir, "src"), dirs_exist_ok=True)
+    else:
+        print(f"Backend src directory not found: {backend_src_dir}")
+
+    # Copy the generics folder to the temporary directory
+    if os.path.exists(generics_dir):
+        shutil.copytree(generics_dir, os.path.join(temp_dir, "src", "generics"), dirs_exist_ok=True)
+        print("Generics folder copied to:", temp_dir)
+    else:
+        print(f"Generics directory not found: {generics_dir}")
+
+    return temp_dir
+
+def package_dockerfile(unique_id, data, dockerfile):
+    if "frontend" in data:
+        temp_dir = copy_generics(unique_id, data["frontend"], "frontends", data["backend"])
+    else:
+        temp_dir = copy_generics(unique_id, data["backend"], "backends", data["backend"])
+    
+    # Write dockerfile to temporary directory
+    dockerfile_path = os.path.join(temp_dir, "src", "dockerfile")
+    with open(dockerfile_path, "w") as file:
+        file.write(dockerfile)
+    
+    # Zip the src folder
+    zip_path = os.path.join(temp_dir, "src.zip")
+    with zipfile.ZipFile(zip_path, "w", zipfile.ZIP_DEFLATED) as zip_file:
+        for root, dirs, files in os.walk(os.path.join(temp_dir, "src")):
+            for file in files:
+                file_path = os.path.join(root, file)
+                zip_file.write(file_path, os.path.relpath(file_path, os.path.join(temp_dir, "src")))
+    
+    packaged_dockerfile = zip_path
+    
+    return packaged_dockerfile
+    
 def get_config(folder_name, directory_path):
     directory = os.path.join(os.getcwd(), directory_path)
     for root, dirs, files in os.walk(directory):
